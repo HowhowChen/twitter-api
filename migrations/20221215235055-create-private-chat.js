201735e955b9e19e34cb5b987a49473a641cff8a
HowardWu5566 <ggg1237890@gmail.com>
2022-12-16 08:00:09 +0800

    feat: add private-chat migration

---
+++ migrations/20221215235055-create-private-chat.js
@@ -0,0 +1,35 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('PrivateChats', {
+      id: {
+        alloNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      senderId: {
+        type: Sequelize.INTEGER
+      },
+      receiverId: {
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('PrivateChats')
+  }
+}
