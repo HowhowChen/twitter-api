20fa79d342138126dbabb50dcf099769a7c36d26
HowardWu5566 <ggg1237890@gmail.com>
2022-12-16 08:21:01 +0800

    feat: add group-chat migration

---
+++ migrations/20221216001616-create-group-chat.js
@@ -0,0 +1,35 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('GroupChats', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      RoomId: {
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('GroupChats')
+  }
+}
