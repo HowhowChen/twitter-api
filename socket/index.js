1279fe7db3338c577a7c4abb33a33c76722812c4
Howhow Chen <debby8698@gmail.com>
2022-12-17 22:55:11 +0800

    feat: init socketio

---
+++ socket/index.js
@@ -0,0 +1,12 @@
+const { authenticatedSocket } = require('../middleware/authentication')
+
+module.exports = io => {
+  /* 監聽連線狀態 */
+  io.use(authenticatedSocket).on('connection', async socket => {
+    const clientsCount = io.engine.clientsCount // 獲取當前連接的客戶端數量
+    const user = socket.user
+    console.log(
+      ` ${user.name} connected and number of connections ${clientsCount}`
+    )
+  })
+}
