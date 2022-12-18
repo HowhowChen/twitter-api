18b4f4173d55690b46069012ad213d0c3ad32592
HowardWu5566 <ggg1237890@gmail.com>
2022-12-18 18:06:20 +0800

    feat: add public chat room seeder

--- seeders/20221216065752-rooms-seed.js
+++ seeders/20221216065752-rooms-seed.js
@@ -3,12 +3,16 @@ const { GROUP_CHAT_AMOUNT } = require('../helpers/seeder-helper')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Rooms', [
-      ...Array.from({ length: GROUP_CHAT_AMOUNT }, (_, i) => ({
-        name: `room${i + 1}`,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }))
+    await queryInterface.bulkInsert('Rooms', [{
+      name: 'Public Chatroom',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    ...Array.from({ length: GROUP_CHAT_AMOUNT }, (_, i) => ({
+      name: `room${i + 1}`,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }))
     ])
   },
 
