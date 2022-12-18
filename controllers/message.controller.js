1613d6dfb69250b32d3b49b43d9733848278d586
Howhow Chen <debby8698@gmail.com>
2022-12-19 02:27:34 +0800

    feat: add privateMessage read, create, update and get unReadCount and lastestMessage

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,5 +1,94 @@
-const messageController = {
+const { PrivateChat, sequelize } = require('../models')
+const { Op } = require('sequelize')
 
+const messageController = {
+  getPrivateMessages: async (senderId, receiverId) => {
+    const getPrivateMessage = await PrivateChat.findAll({
+      where: {
+        [Op.or]: [
+          {
+            [Op.and]: [
+              {
+                senderId: senderId
+              },
+              {
+                receiverId: receiverId
+              }
+            ]
+          },
+          {
+            [Op.and]: [
+              {
+                senderId: receiverId
+              },
+              {
+                receiverId: senderId
+              }
+            ]
+          }
+        ]
+      },
+      raw: true,
+      nest: true,
+      order: [['id', 'ASC']]
+    })
+    return getPrivateMessage
+  },
+  postPrivateMessage: async (senderId, receiverId, content) => {
+    const postPrivateMessage = await PrivateChat.create({
+      senderId,
+      receiverId,
+      content
+    })
+
+    return postPrivateMessage
+  },
+  putPrivateMessageStatus: async (senderId, receiverId) => {
+    await PrivateChat.update(
+      { isRead: false },
+      {
+        where: {
+          senderId: receiverId,
+          receiverId: senderId
+        }
+      }
+    )
+  },
+  getPrivateUnreadMessageCount: async (senderId, receiverId) => {
+    return await PrivateChat.findOne({
+      attributes: [
+        'senderId', 'receiverId', 'content', 'isRead', 'createdAt',
+        [sequelize.literal(`(SELECT COUNT(*) FROM PrivateChats WHERE senderId = ${receiverId} AND receiverId = ${senderId} AND isRead = false)`), 'unReadCount']
+      ],
+      where: {
+        [Op.or]: [
+          {
+            [Op.and]: [
+              {
+                senderId: senderId
+              },
+              {
+                receiverId: receiverId
+              }
+            ]
+          },
+          {
+            [Op.and]: [
+              {
+                senderId: receiverId
+              },
+              {
+                receiverId: senderId
+              }
+            ]
+          }
+        ]
+      },
+      raw: true,
+      nest: true,
+      order: [['createdAt', 'DESC']]
+    })
+  }
 }
 
 module.exports = messageController
--- socket/events/privateRoom.js
+++ socket/events/privateRoom.js
@@ -1,8 +1,52 @@
 const messageController = require('../../controllers/message.controller')
 
 module.exports = (io, socket) => {
-  socket.on('message', data => {
-    console.log(data)
-    socket.emit('message', data)
+  //  一進單人聊天室就讀取歷史訊息
+  socket.on('getPrivateMessage', async data => {
+    try {
+      const { receicerId } = data
+      const senderId = socket.user.id
+
+      //  回傳一個陣列， 讀取訊息在[1]
+      const privateMessage = await Promise.all([
+        messageController.putPrivateMessageStatus(senderId, receicerId),
+        messageController.getPrivateMessages(senderId, receicerId)
+      ])
+
+      socket.emit('getPrivateMessage', privateMessage[1])
+    } catch (err) {
+      socket.on('error', err)
+    }
+  })
+
+  //  點擊送出訊息，存進資料庫，回傳新增訊息給前端appendChild
+  socket.on('postPrivateMessage', async data => {
+    try {
+      const { receicerId, content } = data
+      const senderId = socket.user.id
+
+      const postPrivateMessage = await messageController.postPrivateMessage(senderId, receicerId, content)
+
+      socket.emit('getPrivateMessage', postPrivateMessage)
+    } catch (err) {
+      socket.on('error', err)
+    }
+  })
+
+  //  列表讀取最後一筆訊息及未讀數量
+  socket.on('privateMessageList', async data => {
+    try {
+      const receicerIdList = data
+      const senderId = socket.user.id
+
+      //  列表讀取最後一筆訊息及未讀數量
+      const privateMessageList = await Promise.all(receicerIdList.map(async receicerId => (
+        await messageController.getPrivateUnreadMessageCount(senderId, receicerId.receicerId)
+      )))
+
+      socket.emit('privateMessageList', privateMessageList)
+    } catch (err) {
+      socket.on('error', err)
+    }
   })
 }
