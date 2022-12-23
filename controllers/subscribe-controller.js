585e4ff8f08163acce0aa969818f016848abf6e6
Howhow Chen <debby8698@gmail.com>
2022-12-23 19:48:46 +0800

    feat: add delete subscribe

--- controllers/subscribe-controller.js
+++ controllers/subscribe-controller.js
@@ -43,6 +43,45 @@ const subscribeController = {
     } catch (err) {
       next(err)
     }
+  },
+  deleteSubscribing: async (req, res, next) => {
+    try {
+      const currentUserId = helpers.getUser(req).id
+      const subscribingId = Number(req.params.subscribingId)
+
+      if (subscribingId === currentUserId) {
+        return res.status(422).json({
+          status: 'error',
+          message: '使用者不可以取消訂閱自己.'
+        })
+      }
+
+      const [user, subscribe] = await Promise.all([
+        User.findByPk(currentUserId),
+        Subscribe.findOne({
+          where: {
+            subscriberId: currentUserId,
+            subscribingId
+          }
+        })
+      ])
+
+      // 確認使用者是否存在
+      if (!user || user.role === 'admin') {
+        return res.status(404).json({
+          status: 'error',
+          message: '使用者不存在.'
+        })
+      }
+
+      if (!subscribe) throw new Error('你還沒訂閱喔!')
+
+      await subscribe.destroy()
+
+      res.status(200).json({ status: 'success' })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
--- routes/modules/subscribe.js
+++ routes/modules/subscribe.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const subscribeController = require('../../controllers/subscribe-controller')
 
+router.delete('/:subscribingId', subscribeController.deleteSubscribing)
 router.post('/', subscribeController.addSubscribing)
 
 module.exports = router
