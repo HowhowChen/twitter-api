69cd6f4a1cfbfcf1f8fe39b4e75bfd48245c068a
HowardWu5566 <ggg1237890@gmail.com>
2022-12-23 13:44:53 +0800

    feat: add follow-notice migration

---
+++ migrations/20221223054301-create-follow-notice.js
@@ -0,0 +1,41 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('FollowNotices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      followerId: {
+        type: Sequelize.INTEGER
+      },
+      followingId: {
+        type: Sequelize.INTEGER
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      isRead: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('FollowNotices')
+  }
+}
